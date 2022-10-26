635fe20e10243062891c1bf94ec6c27c9dabfab6
Justin Sunday <justin.sunday@live.com>
2022-10-26 11:05:08 -0400

    Started on the Faction Guardians component

--- src/app/components/faction-guardians/faction-guardians.component.ts
+++ src/app/components/faction-guardians/faction-guardians.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {IFactionGuardians, INIT_FACTION_GUARDIANS} from '../../store/models/RaidCollection/faction-guardians.model';
+import {FactionService} from '../../services/factions/faction.service';
+import {IFactionsByFactionGroup} from '../../store/models/Database/factions-by-faction-group';
 
 @Component({
   selector: 'app-faction-guardians',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FactionGuardiansComponent implements OnInit {
 
-  constructor() { }
+  _initFactionGuardians = INIT_FACTION_GUARDIANS;
+  _factionsByFactionGroup: IFactionsByFactionGroup[] = [];
+
+  get factionsByFactionGroup(){
+    return this._factionsByFactionGroup;
+  }
+
+  @Input()
+  set factionGuardians(value: IFactionGuardians){
+    this._initFactionGuardians = {...this._initFactionGuardians, ...value};
+    console.log(this._initFactionGuardians);
+  }
+
+  get factionGuardians(){
+    return this._initFactionGuardians;
+  }
+
+  constructor(private _factionService: FactionService) { }
 
   ngOnInit(): void {
+    // this._factionsByFactionGroup = this._factionService.getAllActiveFactionsAndGroups();
   }
 
 }
